c82b1cc37bded86cbcc3860a16454717d7aa1892
Andrii543 <andrii.rusyn15@gmail.com>
2025-02-27 22:17:14 +0100

    Calculator Home Work

---
+++ Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_Class_C__Pro
+{
+    public class Calculator
+    {
+
+        private int sumIntegers = 0;
+        private double sumDouble = 0;
+
+        //Метод додавання
+        public int Addition(int num1, int num2) => sumIntegers = num1 + num2;
+
+        //Метод віднімання
+        public int Subtraction(int num1, int num2) => sumIntegers = num1 - num2;
+
+        //Метод ділення
+        public double Division(int num1, int num2)
+        {
+            if (num1 == 0 && num2 == 0)
+            {
+                throw new DivideByZeroException("Ділення на 0 не можливе");
+            }
+            return sumIntegers = num1 / num2;
+            
+        }
+
+        //Метод множення
+        public int Multiplication(int num1, int num2) => sumIntegers = num1 * num2;
+
+        //Метод косінуса за допомогою класу Math який входить в бібліотеку .Net Framework
+        public double Cosinus(int num) => sumDouble = Math.Cos(num);
+
+        //Метод сінуса
+        public double Sinus(int num) => sumDouble = Math.Sin(num);
+
+
+        public void ToString() => Console.WriteLine($"Сумма : {sumIntegers}");
+
+        public void ToStringDouble() => Console.WriteLine($"Cума : {sumDouble:F2}");
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Home_Work_Class_C__Pro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+
+
+            Console.Write("1 число: ");
+            int userInpuntNumberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2 число: ");
+            int userInputNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+            var cal = new Calculator();
+
+            cal.Division(userInpuntNumberOne, userInputNumberTwo);
+
+            cal.ToString();
+
+            cal.Cosinus(userInpuntNumberOne);
+
+            cal.ToStringDouble();
+
+        }
+    }
+}
